2e9965758f53fbcb43cd1ae5e4c69cd6484e31e8
Seth Lawler <slawler@gmu.edu>
2016-06-07 23:57:48 -0400

    Add files via upload

---
+++ SHG_GridMaker_Classes.py
@@ -0,0 +1,59 @@
+from pyproj import Proj
+from numpy import floor as flr
+from numpy import array
+class Grid(object):
+   
+    def __init__(self, grid):
+        self.grid = grid
+        
+    def dec_lats(self):   
+        f = Dataset(nc_file,'r')
+        lats   = f.variables['Latitude'][:]
+        return lats
+    
+    def dec_lons(self):   
+        f = Dataset(nc_file,'r')
+        lons   = f.variables['Longitude'][:]
+        return lons
+        
+    def LowerLeft_LatLon(self): 
+        lats, lons  = self.dec_lats(), self.dec_lons()
+        xmin,ymin = min(lats), min(lons)
+        return xmin, ymin
+              
+    def proj_shift(self,i,j):
+        proj = Proj('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23.0 + \
+                 lon_0=-96 +x_0=0 + y_0=0+ellps=GRS80 +datum=NAD83 + \
+                 units=m +no_defs ')
+        x, y  = proj(i,j)         
+        return x,y        
+        
+    def DecDeg2SHG(self,i,j):
+        x, y  = self.proj_shift(i,j)
+        grid_x, grid_y  = int(flr(x)/250),int(flr(1)/250)  
+        return grid_x, grid_y  
+'''      
+    def GridCoords(self): 
+        lats, lons  = self.dec_lats(), self.dec_lons()
+        x,y = self.DecDeg2SHG(min(lons), min(lats))   
+        return x,y  
+'''        
+    def columns(self): 
+        cols= []
+        lats, lons  = self.dec_lats(), self.dec_lons()
+        for x in lons:
+            colname = self.DecDeg2SHG(x,lats[0])
+            cols.append(colname[0])
+        return array(cols)
+    
+    def rows(self): 
+        rows = []
+        lats, lons  = self.dec_lats(), self.dec_lons()
+        for y in lats:
+            rowname = self.DecDeg2SHG(lons[0],y)
+            rows.append(rowname[0])
+        return array(rows)
+
+
+        
+  
