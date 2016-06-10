d818e41ecafd20091bb5d3307b25d72a8f8021e6
Seth Lawler <slawler@gmu.edu>
2016-06-10 11:43:40 -0400

    Update SHG_GridMaker_Classes.py

--- SHG_GridMaker_Classes.py
+++ SHG_GridMaker_Classes.py
@@ -7,9 +7,9 @@ from numpy import array
 
 class Grid(object):
    
-    def __init__(self, grid):
+    def __init__(self, grid, cellsize):
         self.grid = grid
-        
+        self.cellsize = cellsize
     def dec_lats(self):   
         f = Dataset(self.grid,'r')
         lats   = f.variables['Latitude'][:]
@@ -21,7 +21,7 @@ class Grid(object):
         return lons
         
     def LowerLeft_LatLon(self): 
-        lats, lons  = self.dec_lats(), self.dec_lons()
+        lons, lats  = self.dec_lons(),self.dec_lats()
         xmin,ymin = min(lons), min(lats)
         return xmin, ymin
               
@@ -29,26 +29,38 @@ class Grid(object):
         proj = Proj('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23.0 + \
                  lon_0=-96 +x_0=0 + y_0=0+ellps=GRS80 +datum=NAD83 + \
                  units=m +no_defs ')
-        x, y  = proj(i,j)         
-        return x,y        
-        
+        proj_x, proj_y  = proj(i,j)         
+        return proj_x, proj_y        
+     
     def DecDeg2SHG(self,i,j):
         x, y  = self.proj_shift(i,j)
-        grid_x, grid_y  = int(flr(x)/250),int(flr(1)/250)  
+        grid_x, grid_y  = int(flr(x)/self.cellsize),int(flr(y)/self.cellsize)  
         return grid_x, grid_y  
- 
+
+    def LowerLeft_SHG(self): 
+        ll_x,ll_y = self.LowerLeft_LatLon()
+        shg_ll_x, shg_ll_y = self.DecDeg2SHG(ll_x,ll_y)
+        return shg_ll_x, shg_ll_y   
+        
     def columns(self): 
         cols= []
-        lats, lons  = self.dec_lats(), self.dec_lons()
+        lons, lats  = self.dec_lons(), self.dec_lats()
         for x in lons:
-            colname = self.DecDeg2SHG(x,lats[0])
-            cols.append(colname[0])
+            col, row = self.DecDeg2SHG(x,lats[0])
+            cols.append(col)
         return array(cols)
     
     def rows(self): 
         rows = []
-        lats, lons  = self.dec_lats(), self.dec_lons()
+        lons, lats  = self.dec_lons(), self.dec_lats()
         for y in lats:
-            rowname = self.DecDeg2SHG(lons[0],y)
-            rows.append(rowname[0])
+            col, row = self.DecDeg2SHG(min(lons),y)
+            rows.append(row)
+            print(col,row)
         return array(rows)
+        
+   
+
+
+nc = '29May2015_071200_10min.nc'
+g = Grid(nc,500)
